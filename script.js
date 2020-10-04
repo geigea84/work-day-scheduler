317606735767313fbf4fd64d21723d47d8b993bb
geigea84 <geigea84@gmail.com>
2020-10-03 20:38:47 -0500

    added current time function, need to tweak interval function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 //update #currentDay
 $("#currentDay").text(moment().format("dddd-MMMM-DD"));
 
+//check time to change colors of corresponding time blocks-will also need
+//setInterval to refresh and loop through time-blocks
+
+
+//current time
+var currentTime = moment().format("HH");
+console.log(currentTime);
+
+//setInterval to refresh check for changing time-block colors
+var interval = setInterval(currentTime, 5000)
+console.log(interval);
+
+//translate id string to integer
+
+//if .addClass/.removeClass
+
+//else if
+
+//else
+
+//$(".saveBtn").on("click", function ()
+//); setItem
+
+//getItem
